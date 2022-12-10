18c74883be3119c3e332ba9037c628ab56c4c540
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2022-12-10 20:44:46 +0200

    Task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,11 @@
+// Напиши скрипт, який під час набору тексту в інпуті 
+// input#name-input (подія input), підставляє його 
+// поточне значення в span#name-output.
+//  Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous"
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+nameInput.addEventListener('input', getName);
+function getName(evn){
+    nameOutput.textContent = nameInput.value;
+}
