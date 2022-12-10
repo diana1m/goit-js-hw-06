6e1515560413b9eb8d6d7f2c943cba43298a1218
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2022-12-10 21:17:04 +0200

    Task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,12 @@
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), 
+// перевіряє його вміст щодо правильної кількості введених символів.
+
+const input = document.querySelector('#validation-input');
+input.addEventListener('blur', validation);
+function validation(evn){
+    if (input.value.length < 6) {
+        input.style.borderColor = "#f44336";
+    } else {
+        input.style.borderColor = "#4caf50";
+    }
+}
