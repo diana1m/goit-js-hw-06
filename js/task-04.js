11488bb6c78795a5ad8b6c1e81225cd366d78298
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2022-12-11 22:47:35 +0200

    Update task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,5 @@
 const value = document.querySelector('#value');
-
+let counterValue = 0;
 console.log(value);
 
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
@@ -9,10 +9,11 @@ decrementBtn.addEventListener('click', clickMinus);
 incrementBtn.addEventListener('click', clickPlus);
 
 function clickMinus(evn){
-    value.textContent -= 1;
+    counterValue -=1;
+    value.textContent = counterValue;
 }
 
 function clickPlus(evn){
-   value.textContent = Number(value.textContent) + 1;
+    counterValue +=1;
+    value.textContent = counterValue;
 }
-
